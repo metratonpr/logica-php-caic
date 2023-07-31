0a186291f98c4205af98be5bf4ea61f0a16c26f0
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-31 20:22:09 -0300

    Formulario

--- logica.php
+++ logica.php
@@ -1,3 +1,30 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logica PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <h2>Formulario</h2>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="numero1">Numero 1</label>
+                <input type="number" id="numero1" name="numero1">
+            </div>
+            <div class="mb-3">
+                <label for="numero2">Numero 2</label>
+                <input type="number" id="numero2" name="numero2">
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </main>
+    //Java Script Bootstrap
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>

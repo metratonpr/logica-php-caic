b57bf1ca1c720567ef44e23eb0350ef084a1268b
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-31 20:25:38 -0300

    correção comentario

--- logica.php
+++ logica.php
@@ -27,7 +27,7 @@
             <button type="submit">Enviar</button>
         </form>
     </main>
-    //Java Script Bootstrap
+    <!-- Java Script Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
